d363d0870b5b88b490c71043f89c2164f6ccc2c3
Hanish Jierdh <hanish.jierdh@edu.sait.ca>
2024-09-11 12:14:42 -0600

    Added the week 3 demo page

--- app/page.js
+++ app/page.js
@@ -1,8 +1,12 @@
+import Link from 'next/link';
 export default function Page() {
   return (
     <div>
       <h1>Web Dev 2 Demos</h1>
       <p>Demos for Web Dev 2</p>
+      <li>
+        <Link href="/week-2">Week 2</Link>
+      </li>
     </div>
   );
 }
---
+++ app/week-2/page.js
@@ -0,0 +1,14 @@
+import Heading from './heading.js';
+import Link from 'next/link';
+export default function Page() {
+    let a = 5;
+    let b = 10;
+    return(
+        <main>
+            <Heading />
+            <p>These are the demos for week 2.</p>
+            <p>The sum of {a} + {b} is {a + b}.</p>
+            <p>Go back to <Link href="/">the home page</Link></p>
+        </main>
+    )
+}
