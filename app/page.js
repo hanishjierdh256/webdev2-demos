c6dce63b71064c49736c33830890d7118a698955
Hanish Jierdh <hanish.jierdh@edu.sait.ca>
2024-09-18 12:22:07 -0600

    Updated week 3

--- app/page.js
+++ app/page.js
@@ -7,6 +7,19 @@ export default function Page() {
       <li>
         <Link href="/week-2">Week 2</Link>
       </li>
+      <p>Demos for Web Dev 2</p>
+      <li>
+        <Link href="/week-3">Week 3</Link>
+      </li>
+      <li>
+        <Link href="/week-4">Week 4</Link>
+      </li>
+      <li>
+        <Link href="/week-5">Week 5</Link>
+      </li>
+      <li>
+        <Link href="/week-6">Week 6</Link>
+      </li>
     </div>
   );
 }
